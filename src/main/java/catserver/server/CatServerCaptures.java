47f54c1121c756be9412272353259ec00edb9b4b
Kotori0629 <79943751@qq.com>
2023-02-27 04:01:37 +0800

    Level patch

--- src/main/java/catserver/server/CatServerCaptures.java
+++ src/main/java/catserver/server/CatServerCaptures.java
@@ -3,12 +3,14 @@ package catserver.server;
 import net.minecraft.world.entity.Entity;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class CatServerCaptures {
     private static final CatServerCaptures catServerCaptures = new CatServerCaptures();
     private AtomicReference<Entity> entity = new AtomicReference<>();
     private AtomicReference<CreatureSpawnEvent.SpawnReason> spawnReason = new AtomicReference<>();
+    private AtomicBoolean doPlace = new AtomicBoolean(true);
 
     public void captureEntity(Entity entity) {
         this.entity.set(entity);
@@ -24,6 +26,14 @@ public class CatServerCaptures {
 
     public CreatureSpawnEvent.SpawnReason getCaptureSpawnReason() {
         return this.spawnReason.getAndSet(null);
+    }
+
+    public void captureDoPlace(boolean doPlace) {
+        this.doPlace.set(doPlace);
+    }
+
+    public boolean getCaptureDoPlace() {
+        return this.doPlace.getAndSet(true);
     }
 
     public static CatServerCaptures getCatServerCaptures() {
